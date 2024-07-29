23837f47cd66c85b3aa588ec0c62274a855768bb
ZWilliampR <148885899+ZWilliampR@users.noreply.github.com>
2024-07-28 19:58:44 -0500

    Add files via upload

---
+++ CrearCuenta.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const CrearCuenta = () => {
+	return (
+		<View>
+			<Text>Crear Cuenta Pagina</Text>
+		</View>
+	);
+};
+
+export default CrearCuenta;
